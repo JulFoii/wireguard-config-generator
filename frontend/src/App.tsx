30dc22386d81c46c72d0a74f1325ce4057bcba01
Julfoi <89031080+JulFoii@users.noreply.github.com>
2025-02-04 22:38:14 +0100

    Create App.tsx

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>WireGuard Config Generator (Frontend)</h1>
+      <p>Hello, world!</p>
+    </div>
+  );
+}
+
+export default App;
